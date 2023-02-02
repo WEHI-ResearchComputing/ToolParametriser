bbfe0f8507aeed4ff9a7c3335bb39f007e489f3b
Julie Iskander <iskander.j@wehi.edu.au>
2023-02-02 21:34:32 +1100

    fix run.py

--- ToolParametriser/run.py
+++ ToolParametriser/run.py
@@ -1,19 +1,23 @@
 #!/stornext/HPCScratch/home/iskander.j/myenvs/py3_11/bin/python
+
 import testcreator
 import tomllib
 import logging,os,errno
 
-if not os.path.exists(f'{os.path.expanduser("~")}/.toolparametriser/'):
-    os.makedirs(f'{os.path.expanduser("~")}/.toolparametriser/')
+def main():
+    if not os.path.exists(f'{os.path.expanduser("~")}/.toolparametriser/'):
+        os.makedirs(f'{os.path.expanduser("~")}/.toolparametriser/')
 
-config_path="/vast/scratch/users/iskander.j/ToolParametriser/examples/configMQ.toml"
-if not os.path.exists(config_path):
-    logging.fatal("Config file does not exit")
-    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), config_path)
-        
-with open(config_path, "rb") as f:
-    config = tomllib.load(f)   
-    #print(config)
+    config_path="/vast/scratch/users/iskander.j/ToolParametriser/examples/configMQ.toml"
+    if not os.path.exists(config_path):
+        logging.fatal("Config file does not exit")
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), config_path)
+            
+    with open(config_path, "rb") as f:
+        config = tomllib.load(f)   
 
-mq=testcreator.MQTester(config=config)
-mq.run_test(usetmpl=False)
+    mq=testcreator.MQTester(config=config)
+    mq.run_test(usetmpl=False)
+
+if __name__ == "__main__":
+    main()
